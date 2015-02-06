77aa84d28e85dfb46dcaac226970db92844a25a6
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-02-06 10:38:01 -0300

    Added a comment in default attributes file specifying default values.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,10 @@ default["mo_backup_sample"]["id"] = "my_app"
 default["mo_backup_sample"]["description"] = "My app"
 default["mo_backup_sample"]["user"] = "vagrant"
 default["mo_backup_sample"]["databag"] = "applications"
+
+default["mo_backup_sample"]["backup"]["encryption_databag"] = "backup_encryption"
+
+# Following attributes are optional, have default values
 default["mo_backup_sample"]["backup"]["storages_databag"] = "backup_storages"
 default["mo_backup_sample"]["backup"]["syncers_databag"] = "backup_syncers"
-default["mo_backup_sample"]["backup"]["encryption_databag"] = "backup_encryption"
 default["mo_backup_sample"]["backup"]["mail_databag"] = "mail_databag"
