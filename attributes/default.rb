09cfdb3440a91b150a0729592d0f77b505a7af9b
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-02-13 14:36:18 -0300

    Removed attributes which have default values.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,8 +4,3 @@ default["mo_backup_sample"]["user"] = "vagrant"
 default["mo_backup_sample"]["databag"] = "applications"
 
 default["mo_backup_sample"]["backup"]["encryption_databag"] = "backup_encryption"
-
-# Following attributes are optional, have default values
-default["mo_backup_sample"]["backup"]["storages_databag"] = "backup_storages"
-default["mo_backup_sample"]["backup"]["syncers_databag"] = "backup_syncers"
-default["mo_backup_sample"]["backup"]["mail_databag"] = "mail_databag"
